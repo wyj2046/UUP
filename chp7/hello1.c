7b07901330747c85e67dd4948bf8eac81bad7bdc
Wang yujie <wyj2046@gmail.com>
2012-07-28 19:13:44 +0800

    commit chp7

---
+++ chp7/hello1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <curses.h>
+
+int main(int argc, char* argv[])
+{
+    initscr();
+    clear();
+    move(10, 20);
+    addstr("hello, world!");
+    move(LINES-1, 0);
+    refresh();
+    getch();
+    endwin();
+    return 0;
+}
